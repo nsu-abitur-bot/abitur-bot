7890b1ded931443ebd5470ad0b4c4a4ad196917f
Marat <m.timofeev1@g.nsu.ru>
2025-11-05 09:43:14 +0700

    Починил тесты

--- rag/loader.py
+++ rag/loader.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from rag.vectorstore import get_vectorstore
 
