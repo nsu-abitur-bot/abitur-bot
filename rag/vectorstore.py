bfe17c4dbbef00dec45729da757542c78417c1c3
Marat <m.timofeev1@g.nsu.ru>
2025-12-02 21:40:04 +0700

    Закешировал объект Chroma

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -4,12 +4,19 @@ from rag.embeddings import get_embedding_model
 
 PERSIST_DIR = "./chroma_db"
 
+# Кешируем объект базы данных
+_vectorstore_instance = None
+
 
 def get_vectorstore():
-    embedding = get_embedding_model()
-    db = Chroma(
-        collection_name="abitur_knowledge_base",
-        embedding_function=embedding,
-        persist_directory=PERSIST_DIR,
-    )
-    return db
+    global _vectorstore_instance
+
+    if _vectorstore_instance is None:
+        embedding = get_embedding_model()
+        _vectorstore_instance = Chroma(
+            collection_name="abitur_knowledge_base",
+            embedding_function=embedding,
+            persist_directory=PERSIST_DIR,
+        )
+
+    return _vectorstore_instance
